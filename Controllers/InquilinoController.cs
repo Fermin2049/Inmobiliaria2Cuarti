f370528f6d54efe1d76148c2c1eff2165d02999b
Santiago Farioli <santiago8773cba@gmail.com>
2024-08-31 16:47:43 -0300

    Inquilino listo

---
+++ Controllers/InquilinoController.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Mvc;
+using Inmobiliaria2Cuatri.Models;
+using System.Diagnostics;
+
+namespace Inmobiliaria2Cuatri.Controllers
+{
+    public class InquilinoController : Controller
+    {
+        private readonly ILogger<InquilinoController> _logger;
+        private RepositorioInquilino repo;
+
+        public InquilinoController(ILogger<InquilinoController> logger)
+        {
+            _logger = logger;
+            repo = new RepositorioInquilino();
+        }
+
+        public IActionResult Index()
+        {
+            var lista = repo.ObtenerTodos();
+            return View(lista); 
+        }
+
+        // Método para mostrar el formulario de edición
+        public IActionResult Edicion(int id)
+        {
+            if(id == 0)
+                return View();
+            else
+             {
+                var inquilino = repo.Obtener(id);
+                return View(inquilino);
+             }
+        }
+
+        // Método para manejar el envío del formulario de edicion
+        [HttpPost]
+        public IActionResult Edicion(Inquilino inquilino)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.ActualizarInquilino(inquilino);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(inquilino);
+        }
+
+        // Método para mostrar el formulario de creación
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        // Método para manejar el envío del formulario de creacion
+        [HttpPost]
+        public IActionResult Crear(Inquilino inquilino)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.CrearInquilino(inquilino);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(inquilino);
+        }
+
+        public IActionResult Eliminar(int id)
+        {
+            if (id != 0)
+            {
+                repo.EliminarLogico(id);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
+
---
+++ Models/RepositorioInquilino.cs
@@ -0,0 +1,164 @@
+using MySql.Data.MySqlClient;
+
+namespace Inmobiliaria2Cuatri.Models;
+    public class RepositorioInquilino
+    {
+        string ConectionString = "Server=localhost;User Id=root;Password=;Database=inmobiliaria2;";
+
+        public List<Inquilino> ObtenerTodos()
+        {
+            List<Inquilino> inquilino = new List<Inquilino>();
+            using(MySqlConnection connection = new MySqlConnection(ConectionString))
+            {
+                var query = $@"SELECT {nameof(Inquilino.idInquilino)},
+                                      {nameof(Inquilino.Nombre)},
+                                      {nameof(Inquilino.Apellido)},
+                                      {nameof(Inquilino.Dni)},
+                                      {nameof(Inquilino.Email)},
+                                      {nameof(Inquilino.Telefono)},
+                                      {nameof(Inquilino.Estado)}
+                            FROM inquilino
+                            WHERE {nameof(Inquilino.Estado)} = true"; // Solo traer activos"; 
+                using(MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        inquilino.Add(new Inquilino
+                        {
+                            idInquilino = reader.GetInt32(nameof(Inquilino.idInquilino)),
+                            Nombre = reader.GetString(nameof(Inquilino.Nombre)),
+                            Apellido = reader.GetString(nameof(Inquilino.Apellido)),
+                            Dni = reader.GetInt32(nameof(Inquilino.Dni)),
+                            Email = reader.GetString(nameof(Inquilino.Email)),
+                            Telefono = reader.GetString(nameof(Inquilino.Telefono)),
+                            Estado = reader.GetBoolean(nameof(Inquilino.Estado))
+                        });
+                    }
+                    connection.Close();
+                }
+                return inquilino;
+            }
+        }
+
+        public Inquilino? Obtener(int id)
+        {
+            Inquilino? res = null;
+            using(MySqlConnection connection = new MySqlConnection(ConectionString))
+            {
+                var query = $@"SELECT {nameof(Inquilino.idInquilino)},
+                                      {nameof(Inquilino.Nombre)},
+                                      {nameof(Inquilino.Apellido)},
+                                      {nameof(Inquilino.Dni)},
+                                      {nameof(Inquilino.Email)},
+                                      {nameof(Inquilino.Telefono)},
+                                      {nameof(Inquilino.Estado)}
+                            FROM inquilino
+                            WHERE {nameof(Inquilino.idInquilino)} = @idInquilino";
+                            
+                using(MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@idInquilino", id);
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+                    if(reader.Read())
+                    {
+                        res = new Inquilino
+                        {
+                            idInquilino = reader.GetInt32(nameof(Inquilino.idInquilino)),
+                            Nombre = reader.GetString(nameof(Inquilino.Nombre)),
+                            Apellido = reader.GetString(nameof(Inquilino.Apellido)),
+                            Dni = reader.GetInt32(nameof(Inquilino.Dni)),
+                            Email = reader.GetString(nameof(Inquilino.Email)),
+                            Telefono = reader.GetString(nameof(Inquilino.Telefono)),
+                            Estado = reader.GetBoolean(nameof(Inquilino.Estado))
+                        };
+                    }
+                    connection.Close();
+                }
+                return res;
+            }
+        }
+
+        public int CrearInquilino(Inquilino inquilino)
+        {
+            int res = 0;
+            using (MySqlConnection connection = new MySqlConnection(ConectionString))
+            {
+                var query = @$"INSERT INTO inquilino 
+                            ({nameof(Inquilino.Nombre)}, 
+                             {nameof(Inquilino.Apellido)}, 
+                             {nameof(Inquilino.Dni)}, 
+                             {nameof(Inquilino.Email)}, 
+                             {nameof(Inquilino.Telefono)},
+                             {nameof(Inquilino.Estado)}) 
+                         VALUES (@Nombre, @Apellido, @Dni, @Email, @Telefono, @Estado); 
+                         SELECT LAST_INSERT_ID();";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", inquilino.Nombre);
+                    command.Parameters.AddWithValue("@Apellido", inquilino.Apellido);
+                    command.Parameters.AddWithValue("@Dni", inquilino.Dni);
+                    command.Parameters.AddWithValue("@Email", inquilino.Email);
+                    command.Parameters.AddWithValue("@Telefono", inquilino.Telefono);
+                    command.Parameters.AddWithValue("@Estado", inquilino.Estado);
+             
+                    connection.Open();
+                    res = Convert.ToInt32(command.ExecuteScalar());
+                }
+            } 
+            return res;
+        }
+
+    public bool ActualizarInquilino(Inquilino inquilino)
+    {
+        
+        using (MySqlConnection connection = new MySqlConnection(ConectionString))
+        {
+            var sql = @$"UPDATE inquilino 
+                         SET {nameof(Inquilino.Nombre)} = @Nombre, 
+                             {nameof(Inquilino.Apellido)} = @Apellido, 
+                             {nameof(Inquilino.Dni)} = @Dni, 
+                             {nameof(Inquilino.Email)} = @Email, 
+                             {nameof(Inquilino.Telefono)} = @Telefono,
+                             {nameof(Inquilino.Estado)} = @Estado
+                         WHERE {nameof(Inquilino.idInquilino)} = @idInquilino;";
+            using (MySqlCommand command = new MySqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@idInquilino", inquilino.idInquilino);
+                command.Parameters.AddWithValue("@Nombre", inquilino.Nombre);
+                command.Parameters.AddWithValue("@Apellido", inquilino.Apellido);
+                command.Parameters.AddWithValue("@Dni", inquilino.Dni);
+                command.Parameters.AddWithValue("@Email", inquilino.Email);
+                command.Parameters.AddWithValue("@Telefono", inquilino.Telefono);
+                command.Parameters.AddWithValue("@Estado", inquilino.Estado);
+                
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                return result > 0;
+                
+            }
+        }
+        
+    }
+
+    public int EliminarLogico(int id)
+    {
+        using (MySqlConnection connection = new MySqlConnection(ConectionString))
+        {
+            string sql = @$"UPDATE inquilino 
+                            SET {nameof(Inquilino.Estado)} = @Estado 
+                            WHERE {nameof(Inquilino.idInquilino)} = @idInquilino;";
+            using (MySqlCommand command = new MySqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@idInquilino", id);
+                command.Parameters.AddWithValue("@Estado", false); // Marcar como eliminado lógico
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                connection.Close();
+                return result;
+            }
+        }
+    }
+}
