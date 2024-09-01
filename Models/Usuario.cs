d846de55abc8375a15ff1aca1451d22b8cfba175
Santiago Farioli <santiago8773cba@gmail.com>
2024-09-01 11:59:44 -0300

    mas nullable

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -7,13 +7,13 @@ namespace Inmobiliaria2Cuarti.Models
     public class Usuario
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Email { get; set; }
-        public string Clave { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Email { get; set; }
+        public string? Clave { get; set; }
 
         [Required, DataType(DataType.Password)]
-        public string Avatar { get; set; }
-        public string Rol { get; set; }
+        public string? Avatar { get; set; }
+        public string? Rol { get; set; }
     }
 }
