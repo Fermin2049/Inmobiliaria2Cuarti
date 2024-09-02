cb7649014bf3e0965ff14de238b43ce8adacd07d
Santiago Farioli <santiago8773cba@gmail.com>
2024-09-02 10:22:38 -0300

    Usuario

--- obj/Debug/net8.0/MuebleriaSegundoCuatri.AssemblyInfo.cs
+++ obj/Debug/net8.0/MuebleriaSegundoCuatri.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuebleriaSegundoCuatri")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72b1266bed658c4a1eb24e7862e8e314d290d992")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff1b7706970954eb7b1451c3b0cd370ec60541e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuebleriaSegundoCuatri")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuebleriaSegundoCuatri")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
