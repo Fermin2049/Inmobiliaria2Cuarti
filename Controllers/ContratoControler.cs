2455b6492a014ab146bbdbb5b3596e71fd34a392
Fermin2049 <fermin2049@gmail.com>
2024-09-03 01:23:41 -0300

    contrato 80%

---
+++ Controllers/ContratoControler.cs
@@ -0,0 +1,101 @@
+using System.Diagnostics;
+using Inmobiliaria2Cuarti.Models;
+using Inmobiliaria2Cuatri.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Inmobiliaria2Cuarti.Controllers
+{
+    public class ContratoController : Controller
+    {
+        private readonly ILogger<ContratoController> _logger;
+        private RepositorioContrato repo;
+        private RepositorioInmueble repoInmueble;
+        private RepositorioInquilino repoInquilino;
+
+        public ContratoController(ILogger<ContratoController> logger)
+        {
+            _logger = logger;
+            repo = new RepositorioContrato();
+            repoInmueble = new RepositorioInmueble();
+            repoInquilino = new RepositorioInquilino();
+        }
+
+        public IActionResult Index()
+        {
+            var lista = repo.ObtenerTodos();
+            return View(lista);
+        }
+
+        // Método para mostrar el formulario de edición
+        public IActionResult Edicion(int id)
+        {
+            if (id == 0)
+                return View();
+            else
+            {
+                var contrato = repo.Obtener(id);
+                return View(contrato);
+            }
+        }
+
+        // Método para manejar el envío del formulario de edicion
+        [HttpPost]
+        public IActionResult Edicion(Contrato contrato)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.ActualizarContrato(contrato);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contrato);
+        }
+
+        // Método para mostrar el formulario de creación
+        public IActionResult Crear()
+        {
+            ViewBag.Inmuebles = new SelectList(
+                repoInmueble.ObtenerTodos(),
+                "IdInmueble",
+                "Direccion"
+            );
+            ViewBag.Inquilinos = new SelectList(
+                repoInquilino.ObtenerTodos(),
+                "idInquilino",
+                "Nombre"
+            );
+            return View();
+        }
+
+        // Método para manejar el envío del formulario de creacion
+        [HttpPost]
+        public IActionResult Crear(Contrato contrato)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.CrearContrato(contrato);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Inmuebles = new SelectList(
+                repoInmueble.ObtenerTodos(),
+                "IdInmueble",
+                "Direccion"
+            );
+            ViewBag.Inquilinos = new SelectList(
+                repoInquilino.ObtenerTodos(),
+                "idInquilino",
+                "Nombre"
+            );
+            return View(contrato);
+        }
+
+        public IActionResult Eliminar(int id)
+        {
+            if (id != 0)
+            {
+                repo.EliminarLogico(id);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
