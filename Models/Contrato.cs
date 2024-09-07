12312451169c61ba67b8c334414626f2882ef322
Fermin2049 <fermin2049@gmail.com>
2024-09-07 10:13:48 -0300

    caombio de muestra en la tabla contratos

--- Models/Contrato.cs
+++ Models/Contrato.cs
@@ -1,36 +1,41 @@
-using System;
-namespace Inmobiliaria2Cuarti.Models;
 using System.ComponentModel.DataAnnotations;
 
-    public class Contrato
-    {
-        [Key]
-        public int IdContrato { get; set; }
-        [Required]
-        public int IdInmueble { get; set; }
-        [Required]
-        public int IdInquilino { get; set; }
-        [Required]
+namespace Inmobiliaria2Cuarti.Models;
 
-        [DataType(DataType.Date)]
-        public DateTime FechaInicio { get; set; }
-        [Required]
+public class Contrato
+{
+    [Key]
+    public int IdContrato { get; set; }
 
-        [DataType(DataType.Date)]
-        public DateTime FechaFin { get; set; }
-        [Required]
+    [Required]
+    public int IdInmueble { get; set; }
 
-        [DataType(DataType.Currency)]
-        public decimal MontoRenta { get; set; }
+    [Required]
+    public int IdInquilino { get; set; }
 
-        [DataType(DataType.Currency)]
-        public decimal Deposito { get; set; }
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime FechaInicio { get; set; }
 
-        [DataType(DataType.Currency)]
-        public decimal Comision { get; set; }
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime FechaFin { get; set; }
 
-        public string? Condiciones { get; set; }
-    }
+    [Required]
+    [DataType(DataType.Currency)]
+    public decimal MontoRenta { get; set; }
 
+    [DataType(DataType.Currency)]
+    public decimal Deposito { get; set; }
 
+    [DataType(DataType.Currency)]
+    public decimal Comision { get; set; }
+    public string? Condiciones { get; set; }
 
+    // propiedades para mostrar datos
+    public string? PropietarioNombre { get; set; }
+    public string? PropietarioApellido { get; set; }
+    public string? InmuebleDireccion { get; set; }
+    public string? InquilinoNombre { get; set; }
+    public string? InquilinoApellido { get; set; }
+}
