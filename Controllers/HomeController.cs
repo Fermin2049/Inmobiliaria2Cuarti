67e026438ddf3d0d3359f3f38368d46e16f9d805
Fermin2049 <fermin2049@gmail.com>
2024-09-20 20:50:28 -0300

    Dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Inmobiliaria2Cuarti.Models;
 using Inmobiliaria2Cuatri.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,15 +10,30 @@ namespace Inmobiliaria2Cuatri.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly RepositorioPropietario _repoPropietario;
+        private readonly RepositorioInquilino _repoInquilino;
+        private readonly RepositorioInmueble _repoInmueble;
+        private readonly RepositorioUsuario _repoUsuario;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _repoPropietario = new RepositorioPropietario();
+            _repoInquilino = new RepositorioInquilino();
+            _repoInmueble = new RepositorioInmueble();
+            _repoUsuario = new RepositorioUsuario();
         }
 
         public IActionResult Index()
         {
-            return View();
+            var model = new DashboardViewModel
+            {
+                TotalPropietarios = _repoPropietario.ObtenerTodos().Count(),
+                TotalInquilinos = _repoInquilino.ObtenerTodos().Count(),
+                TotalInmuebles = _repoInmueble.ObtenerTodos().Count(),
+                TotalUsuarios = _repoUsuario.ObtenerTodos().Count(),
+            };
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+namespace Inmobiliaria2Cuatri.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalPropietarios { get; set; }
+        public int TotalInquilinos { get; set; }
+        public int TotalInmuebles { get; set; }
+        public int TotalUsuarios { get; set; }
+    }
+}
