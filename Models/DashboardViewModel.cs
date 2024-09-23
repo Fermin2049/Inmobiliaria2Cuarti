de37ba146d0ad7e9e89642d4e911f290c205526b
Fermin2049 <fermin2049@gmail.com>
2024-09-23 12:09:59 -0300

    correcciones de adventencias

--- Models/DashboardViewModel.cs
+++ Models/DashboardViewModel.cs
@@ -6,8 +6,8 @@ namespace Inmobiliaria2Cuatri.Models
         public int TotalInquilinos { get; set; }
         public int TotalInmuebles { get; set; }
         public int TotalUsuarios { get; set; }
-        public List<int> InquilinosPorInmueble { get; set; }
-        public List<int> PagosMensuales { get; set; }
-        public List<int> NuevosContratosPorMes { get; set; }
+        public List<int> InquilinosPorInmueble { get; set; } = new List<int>();
+        public List<int> PagosMensuales { get; set; } = new List<int>();
+        public List<int> NuevosContratosPorMes { get; set; } = new List<int>();
     }
 }
